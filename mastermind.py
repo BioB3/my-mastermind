3e5e53168a53cceb78ff7ed5fe21fc2c8fd19a20
Phasin Sae-ngow <bird.biob3@gmail.com>
2023-11-16 10:21:39 +0700

    add menu

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,4 @@
-import random, copy
+import random, copy, sys
 class Mastermind:
     def __init__(self):
         self.__column = 4
@@ -23,18 +23,20 @@ class Mastermind:
     def last_guess(self):
         return self.__last_guess
 
-    def setup(self):
-        position = int(input("Enter number of positions (between 1 and 10) : "))
-        num_color = int(input("Enter number of colors (between 1 and 8) : "))
-        if 1 < position < 10 and 1 < num_color < 8:
-            self.__column = position
-            self.__color = num_color
-            print("positions and colors setted")
-            temp_lst = [count for count in range(1, self.__color +1)]
-            self.__answer = random.choices(temp_lst, k = self.__column)
+    def __setup(self):
+        position = (input("Enter number of positions (between 1 and 10) : "))
+        num_color = (input("Enter number of colors (between 1 and 8) : "))
+        if not position.isdigit() or not num_color.isdigit():
+            print("please input NUMBERS.")
+            self.__setup()
+        elif 1 < int(position) < 10 and 1 < int(num_color) < 8:
+            self.__column = int(position)
+            self.__color = int(num_color)
+            print("positions and colors setted\n")
         else:
             print("please try again with the valid numbers.")
-            self.setup()
+            self.__setup()
+        self.menu()
 
     def __gen_hint(self, ans_lst):
         hint_lst = []
@@ -53,9 +55,15 @@ class Mastermind:
         print("".join(hint_lst))
         print()
 
-    def play(self):
+    def __play(self):
+        temp_lst = [count for count in range(1, self.__color +1)]
+        self.__answer = random.choices(temp_lst, k = self.__column)
+        print(f"Playing Mastermind with {self.__color} colors and {self.__column} positions")
         guess = input("What is your guess?: ")
-        if len(guess) == self.__column:
+        if not guess.isdigit():
+            print("please input NUMBERS.")
+            self.__play()
+        elif len(guess) == self.__column:
             ans_lst = [int(i) for i in guess]
             self.__last_guess = guess
             self.__gen_hint(ans_lst)
@@ -65,12 +73,30 @@ class Mastermind:
                 self.__round += 1
                 ans_lst = [int(i) for i in guess]
                 self.__gen_hint(ans_lst)
-            print(f"You solve it after {self.__round} rounds")
+            print(f"You solve it after {self.__round} rounds\nReturning to menu\n")
+            self.menu()
+            
         else:
             print("please enter guess with the correct amount of positions")
-            self.play()
+            self.__play()
 
+    def menu(self):
+        print("\nWelcome to Mastermind! Please pick one of the choices below.\n")
+        print("|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|")
+        print("| Play -- Play Mastermind                                             |")
+        print("| Setup -- config the numbers of colors and position (default is 4x4) |")
+        print("| Quit -- Quit the game                                               |")
+        print("|_____________________________________________________________________|\n")
+        choice = input()
+        if choice.lower() == "play":
+            self.__play()
+        elif choice.lower() == "setup":
+            self.__setup()
+        elif choice.lower() == "quit":
+            sys.exit()
+        else:
+            self.menu()
 
-game = Mastermind()
-game.setup()
-game.play()
+if __name__ == __name__:
+    game = Mastermind()
+    game.menu()
