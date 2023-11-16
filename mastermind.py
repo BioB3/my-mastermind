6a96fa7f8dedc5bda507d9662a219f20de19b82d
Phasin Sae-ngow <bird.biob3@gmail.com>
2023-11-16 09:42:01 +0700

    you can actually play the game

--- mastermind.py
+++ mastermind.py
@@ -1,9 +1,11 @@
-import random
+import random, copy
 class Mastermind:
     def __init__(self):
         self.__column = 4
         self.__color = 4
         self.__answer = []
+        self.__last_guess = ""
+        self.__round = 1
     
     @property
     def column(self):
@@ -17,6 +19,10 @@ class Mastermind:
     def answer(self):
         return self.__answer
 
+    @property
+    def last_guess(self):
+        return self.__last_guess
+
     def setup(self):
         position = int(input("Enter number of positions (between 1 and 10) : "))
         num_color = int(input("Enter number of colors (between 1 and 8) : "))
@@ -24,24 +30,47 @@ class Mastermind:
             self.__column = position
             self.__color = num_color
             print("positions and colors setted")
+            temp_lst = [count for count in range(1, self.__color +1)]
+            self.__answer = random.choices(temp_lst, k = self.__column)
         else:
             print("please try again with the valid numbers.")
             self.setup()
-        temp_lst = [count for count in range(1, self.__color +1)]
-        self.__answer = random.choices(temp_lst, k = self.__column)
-        print(self.__answer)
 
-    def gen_hint(self, ans_lst):
-        temp_lst = []
-        for ans in ans_lst:
-            if ans in self.__answer and ans == self.__answer[ans_lst.index(ans)]:
-                temp_lst.append("*")
-            elif ans in self.__answer:
-                temp_lst.append("o")
-            temp_lst.sort()
-        print(temp_lst)
-        
+    def __gen_hint(self, ans_lst):
+        hint_lst = []
+        _lst = copy.deepcopy(self.__answer)
+        _lst2 = copy.deepcopy(ans_lst)
+        for n in range(len(ans_lst)):
+            if ans_lst[n] == self.__answer[n]:
+                hint_lst.append("*")
+                _lst.remove(self.__answer[n])
+                _lst2.remove(self.__answer[n])
+        for m in _lst2:
+            if m in _lst:
+                hint_lst.append("o")
+        hint_lst.sort()
+        print(f"Your guess is {self.__last_guess}")
+        print("".join(hint_lst))
+        print()
+
+    def play(self):
+        guess = input("What is your guess?: ")
+        if len(guess) == self.__column:
+            ans_lst = [int(i) for i in guess]
+            self.__last_guess = guess
+            self.__gen_hint(ans_lst)
+            while ans_lst != self.answer:
+                guess = input("What is your guess?: ")
+                self.__last_guess = guess
+                self.__round += 1
+                ans_lst = [int(i) for i in guess]
+                self.__gen_hint(ans_lst)
+            print(f"You solve it after {self.__round} rounds")
+        else:
+            print("please enter guess with the correct amount of positions")
+            self.play()
 
 
 game = Mastermind()
 game.setup()
+game.play()
